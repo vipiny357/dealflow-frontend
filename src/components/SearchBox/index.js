dc82b8bfdcb7a17a52ecd9af18a80801ad5aadcb
Vipin Kumar Yadav <62796627+vipiny357@users.noreply.github.com>
2023-07-13 13:58:50 +0530

    ip update

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -32,7 +32,7 @@ const SearchBox = ({ pageNumber, pageSize, setData, setIsLoading }) => {
 
       if(!queryParams) return;
 
-    const url = `https://18.119.102.108/freelancers/search?${queryParams}&page_number=${pageNumber}&page_size=${pageSize}`;
+    const url = `https://18.188.92.21/freelancers/search?${queryParams}&page_number=${pageNumber}&page_size=${pageSize}`;
     setIsLoading(true)
     fetch(url)
       .then((response) => {
